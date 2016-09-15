75acd57229d3a4138a0640ce73dd62ed20b903df
IAlamOfficial <ialam5539@bths.edu>
2016-09-15 13:18:56 -0400

    added her grade

--- Introduction/src/introUnit/ProceduralExample.java
+++ Introduction/src/introUnit/ProceduralExample.java
@@ -10,6 +10,8 @@ package introUnit;
 public class ProceduralExample {
 	public static void main(String[] args){
 		String[] names = {"Jillian","Joseph","Jordan"};
-		System.out.println("My Name is " + names[0] + ".");
+		String[] grade = {"Senior","Junior","Frehman"};
+		System.out.println("My Name is " + names[0] + ", and I am a " + grade[0] + ".");
+		
 	}
 }
